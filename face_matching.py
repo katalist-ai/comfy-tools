0358e372dcd678c3b1e9179b1a45adf391dc0d48
Fathaah <abdulfta12@gmail.com>
2024-10-12 12:10:44 +0200

    added old face matcher to start testing side by side

--- face_matching.py
+++ face_matching.py
@@ -7,21 +7,132 @@ import scipy
 import torch
 from folder_paths import models_dir, folder_names_and_paths
 from scipy.stats import entropy
-from insightface.app import FaceAnalysis
-import torchvision.transforms.v2 as T
 
-INSIGHTFACE_DIR = os.path.join(models_dir, "insightface")
+
+def add_folder_path_and_extensions(folder_name, full_folder_paths, extensions):
+    if folder_name in folder_names_and_paths:
+        folder_names_and_paths[folder_name][0].extend(full_folder_paths)
+        folder_names_and_paths[folder_name][1].update(extensions)
+    else:
+        folder_names_and_paths[folder_name] = (full_folder_paths, set(extensions))
+
+
+add_folder_path_and_extensions("onnx", [os.path.join(models_dir, "onnx")], ['.onnx'])
+
+RESNET_MEAN = np.array([0.485, 0.456, 0.406], dtype=np.float32).reshape(1, 3, 1, 1)
+RESNET_STD = np.array([0.229, 0.224, 0.225], dtype=np.float32).reshape(1, 3, 1, 1)
+
+
+def preprocess_image(image_batch: list, resize=None):
+    """
+    turn list of torch images into a numpy batch
+    list of RGB images in the format [B, H, W, C], numpy ndarray, float32"""
+    if len(image_batch) == 0:
+        return []
+    if resize is None:
+        resize = image_batch[0].shape[1:3]
+    w, h = resize
+    image_batch = np.array([cv2.resize(image.numpy()[0], (w, h)) for image in image_batch])
+    image_batch = image_batch.transpose((0, 3, 1, 2))  # BCHW format
+    b = image_batch.shape[0]
+    # Apply normalization for resnet
+    image_batch = (image_batch - np.repeat(RESNET_MEAN, b, axis=0)) / np.repeat(RESNET_STD, b, axis=0)
+    return image_batch
+
+
+def js_divergence(p, q):
+    p = np.asarray(p)
+    q = np.asarray(q)
+    p /= p.sum()
+    q /= q.sum()
+
+    # Calculate the M distribution
+    m = 0.5 * (p + q)
+
+    # Calculate the Jensen-Shannon Divergence
+    js_div = 0.5 * (entropy(p, m) + entropy(q, m))
+    return js_div
+
+
+def softmax(x):
+    # Subtract the max for numerical stability
+    e_x = np.exp(x - np.max(x, axis=1, keepdims=True))
+    # Divide by the sum of exps
+    return e_x / np.sum(e_x, axis=1, keepdims=True)
+
+
+label_map = {
+    'sex': {
+        0: 'female',
+        1: 'male'
+    },
+    'age': {
+        0: 'adult',
+        1: 'child'
+    }
+}
+
+
+class AgeSexInference:
+    def __init__(self, onnx_model_path):
+        self.model = ort.InferenceSession(onnx_model_path)
+
+    def predict(self, image: np.ndarray):
+        """RGB image in the format [B, C, H, W], numpy ndarray"""
+        age, sex = self.model.run(None, {'face_image': image})
+        return age, sex
+
+    def predict_probs(self, image: np.ndarray):
+        """RGB image in the format [B, C, H, W]"""
+        age, sex = self.predict(image)
+        return softmax(age), softmax(sex)
+
+    def predict_labels(self, image: np.ndarray):
+        """RGB image in the format [B, C, H, W]"""
+        age, sex = self.predict(image)
+        age_idx = np.argmax(age, axis=1)
+        sex_idx = np.argmax(sex, axis=1)
+        return ([label_map['age'][idx] for idx in age_idx],
+                [label_map['sex'][idx] for idx in sex_idx])
+
+
+age_groups = [(8, 0.01), (16, 0.4), (200000000000, 0.99)]
+
+
+def select_age_group(ages):
+    results = []
+    for age in ages:
+        for age_max, prob in age_groups:
+            if age < age_max:
+                results.append([prob, 1 - prob])
+                break
+    return np.array(results).astype(np.float32)
+
+
+class AgeSexInference2:
+    def __init__(self, onnx_model_path):
+        self.model = ort.InferenceSession(onnx_model_path)
+
+    def predict(self, image: np.ndarray):
+        """RGB image in the format [B, C, H, W], numpy ndarray"""
+        out = self.model.run(None, {'data': image})[0]
+        # age in the output is float
+        age = select_age_group(out[:, 2] * 100)
+        sex = softmax(out[:, :2])
+        return age, sex
+
+    def predict_probs(self, image: np.ndarray):
+        """RGB image in the format [B, C, H, W]"""
+        return self.predict(image)
 
 
 class FaceMatcher:
     def __init__(self):
-        self.face_analysis = FaceAnalysis(name="antelopev2", root=INSIGHTFACE_DIR, providers=['CPUExecutionProvider'])
-        self.face_analysis.prepare(ctx_id=0, det_size=(640, 640))
-        self.thresholds = {
-            "cosine": 0.68,
-            "euclidean": 4.15,
-            "L2_norm": 1.13
-        }
+        onnx_model_path = os.path.join(models_dir, "onnx", "model_inception_resnet.onnx")
+        # onnx_models_path = os.path.join(models_dir, 'insightface', 'models', 'buffalo_l', 'genderage.onnx')
+        # age_sex_model_path = os.path.join(folder_names_and_paths['onnx'][0], 'age_sex.onnx')
+        self.model = AgeSexInference(onnx_model_path)
+        # self.model = AgeSexInference2(onnx_models_path)
 
     @classmethod
     def INPUT_TYPES(s):
@@ -38,57 +149,33 @@ class FaceMatcher:
     FUNCTION = "match_faces"
     CATEGORY = "Katalist Tools"
 
-    def get_face(self, image):
-        for size in [(size, size) for size in range(640, 256, -64)]:
-            self.face_analysis.det_model.input_size = size
-            faces = self.face_analysis.get(image)
-            if len(faces) > 0:
-                return sorted(faces, key=lambda x: (x['bbox'][2] - x['bbox'][0]) * (x['bbox'][3] - x['bbox'][1]), reverse=True)
-        return None
-
-    def get_embeds(self, image):
-        face = self.get_face(image)
-        if face is not None:
-            return face[0].normed_embedding
-        return None
-
     def match_faces(self, input_faces: list, target_faces: list):
-        # use insight face to get face for each input and target
-        input_faces = [self.get_embeds(np.array(T.ToPILImage()(face[0].permute(2, 0, 1)).convert('RGB'))) for face in input_faces]
-        target_faces = [self.get_embeds(np.array(T.ToPILImage()(face[0].permute(2, 0, 1)).convert('RGB'))) for face in target_faces]
-
-        # find euclidean distance between each input and target
-        distances = []
-        for i, input_face in enumerate(input_faces):
-            for j, target_face in enumerate(target_faces):
-                dist = np.linalg.norm(input_face - target_face)
-                print(f'the distance between input {i} and target {j} is {dist}')
-                distances.append((dist, i, j))
-        
-        # assign pairs based on least distance
-        distances.sort(key=lambda x: x[0])
-        mapping = [-1] * len(input_faces)
-        used_targets = set()
-        used_inputs = set()
-
-        # First pass: assign each input to the closest available target
-        for dist, input_idx, target_idx in distances:
-            if target_idx not in used_targets and input_idx not in used_inputs:
-                mapping[input_idx] = target_idx
-                used_targets.add(target_idx)
-                used_inputs.add(input_idx)
-
-        # Second pass: ensure all targets are used
-        for target_idx in range(len(target_faces)):
-            if target_idx not in used_targets:
-                for input_idx in range(len(input_faces)):
-                    if mapping[input_idx] == -1:
-                        mapping[input_idx] = target_idx
-                        used_targets.add(target_idx)
-                        break
-
-        print("Face Mapping: ", mapping)
-        return (mapping,)
+        if len(input_faces) == 0 or len(target_faces) == 0:
+            return (None, )
+        # input_faces = torch.cat(input_faces, dim=0)
+        input_faces = preprocess_image(input_faces, resize=(160, 160))
+        # target_faces = torch.cat(target_faces, dim=0)
+        target_faces = preprocess_image(target_faces, resize=(160, 160))
+        age_i, sex_i = self.model.predict_probs(input_faces)
+        age_t, sex_t = self.model.predict_probs(target_faces)
+        print("Age i: ", age_i)
+        print("Age t: ", age_t)
+        print("Sex i", sex_i)
+        print("Sex t", sex_t)
+        distances = np.zeros((len(input_faces), len(target_faces)))
+        for i in range(len(input_faces)):
+            for j in range(len(target_faces)):
+                distances[i, j] = (0.7 * js_divergence(age_i[i], age_t[j]) +
+                                   0.3 * js_divergence(sex_i[i], sex_t[j]))
+        print("Distances: ", distances)
+        rows, cols = scipy.optimize.linear_sum_assignment(distances)
+        mapping = list(zip(rows, cols))
+        mapping = sorted(mapping, key=lambda x: x[0])  # not sure if needed
+        full_mapping = [-1] * len(input_faces)
+        for a, b in mapping:
+            full_mapping[a] = b
+        print("Face Mapping: ", full_mapping)
+        return (full_mapping,)
 
 
 class ShowPermutation:
@@ -107,3 +194,15 @@ class ShowPermutation:
     OUTPUT_NODE = True
     FUNCTION = "show_mapping"
     CATEGORY = "Katalist Tools"
+
+    def show_mapping(self, mapping):
+        print(mapping)
+        return (None,)
+
+
+def main():
+    img = torch.zeros(1, 5, 5, 3)
+
+
+if __name__ == '__main__':
+    main()
