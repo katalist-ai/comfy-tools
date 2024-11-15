2ba8ebce4bfadc0f29d4f690258004c63c78e239
Fathaah <abdulfta12@gmail.com>
2024-11-15 15:58:30 +0100

    just returns pose

--- websocket_pose.py
+++ websocket_pose.py
@@ -17,20 +17,13 @@ class SavePoseWebsocket:
 
     def save_pose(self, pose_keypoint):
         # Create progress bar
-        pbar = comfy.utils.ProgressBar(len(pose_keypoint))
-        
-        for step, pose_dict in enumerate(pose_keypoint):
-            # Convert pose data to a JSON string
-            pose_str = json.dumps({
-                "type": "pose_data",
-                "version": 1,
-                "data": pose_dict
-            })
+        pose_str = json.dumps({
+            "type": "pose_data",
+            "version": 1,
+            "data": pose_keypoint
+        })
             
-            # Update progress bar with the pose string
-            pbar.update_absolute(step, len(pose_keypoint), ("POSE", pose_str))
-
-        return {}
+        return pose_str
 
 NODE_CLASS_MAPPINGS = {
     "SavePoseWebsocket": SavePoseWebsocket,
