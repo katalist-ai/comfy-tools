af7a1e72abf9a8506795d2cbe6a732d2b5aeb32c
Fathaah <abdulfta12@gmail.com>
2024-11-17 18:53:14 +0100

    pose tool update

--- websocket_pose.py
+++ websocket_pose.py
@@ -16,12 +16,16 @@ class SavePoseWebsocket:
     CATEGORY = "api/pose"
 
     def save_pose(self, pose_keypoint):
+        pbar = comfy.utils.ProgressBar(1)
         # Create progress bar
         pose_str = json.dumps({
             "type": "pose_data",
             "version": 1,
             "data": pose_keypoint
         })
+
+        # update progress bar
+        pbar.update_absolute(1, pose_keypoint, ("POSE_KEYPOINT", pose_str, None))
             
         return pose_str
 
